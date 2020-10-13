f4ec5f75345fa3c008b067ed253a6bcd6f9b6ca9
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2020-10-13 12:21:07 +0100

    Lab 2 part 2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+unsigned counter = 0;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -43,9 +44,40 @@ int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
   return 1;
 }
 
-int(timer_test_int)(uint8_t time) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+
+int(timer_test_int)(uint8_t time) {
+  int ipc_status;
+  message msg;
+
+  uint8_t bit_no;
+  timer_subscribe_int(&bit_no);
+
+  uint64_t irq_set = BIT(bit_no);
+  int r;
+  while(time > 0){
+    if((r = driver_receive(ANY, &msg, &ipc_status)) != OK){
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)){
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          if (msg.m_notify.interrupts & irq_set) {
+            timer_int_handler();
+            if(counter % 60 == 0){
+              timer_print_elapsed_time();
+              counter = 0;
+              time--;
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  timer_unsubscribe_int();
+  return 0;
 }
